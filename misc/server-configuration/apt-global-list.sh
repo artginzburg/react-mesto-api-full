8697fae72bd99bfa8c4b2da5086ee7de13bdf0c0
artginzburg <art.ginzburg@ya.ru>
2021-09-10 06:33:32 +0300

    Update apt-global-list.sh

--- misc/server-configuration/apt-global-list.sh
+++ misc/server-configuration/apt-global-list.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-apt install -y nodejs
-apt install -y mongodb-org
-apt install -y git
-apt install -y nginx
-apt install -y certbot python3-certbot-nginx
-apt install -y make
+sudo apt install -y nodejs mongodb-org git nginx certbot python3-certbot-nginx make
